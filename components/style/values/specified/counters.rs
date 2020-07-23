924de9c3f073e49a54be2b35535b0417c1fd9275
Simon Sapin <simon.sapin@exyr.org>
2020-07-23 17:33:50 +0200

    Don’t parse unspported values of `content`

--- components/style/values/specified/counters.rs
+++ components/style/values/specified/counters.rs
@@ -4,7 +4,7 @@
 
 //! Specified types for counter properties.
 
-#[cfg(feature = "servo")]
+#[cfg(feature = "servo-layout-2013")]
 use crate::computed_values::list_style_type::T as ListStyleType;
 use crate::parser::{Parse, ParserContext};
 use crate::values::generics::counters as generics;
@@ -17,7 +17,6 @@ use crate::values::specified::Attr;
 use crate::values::specified::Integer;
 use crate::values::CustomIdent;
 use cssparser::{Parser, Token};
-use selectors::parser::SelectorParseErrorKind;
 use style_traits::{ParseError, StyleParseErrorKind};
 
 /// A specified value for the `counter-increment` property.
@@ -88,7 +87,7 @@ pub type Content = generics::GenericContent<SpecifiedImageUrl>;
 pub type ContentItem = generics::GenericContentItem<SpecifiedImageUrl>;
 
 impl Content {
-    #[cfg(feature = "servo")]
+    #[cfg(feature = "servo-layout-2013")]
     fn parse_counter_style(_: &ParserContext, input: &mut Parser) -> ListStyleType {
         input
             .try_parse(|input| {
@@ -149,12 +148,14 @@ impl Parse for Content {
                 },
                 Ok(&Token::Function(ref name)) => {
                     let result = match_ignore_ascii_case! { &name,
+                        #[cfg(any(feature = "gecko", feature = "servo-layout-2013"))]
                         "counter" => input.parse_nested_block(|input| {
                             let location = input.current_source_location();
                             let name = CustomIdent::from_ident(location, input.expect_ident()?, &[])?;
                             let style = Content::parse_counter_style(context, input);
                             Ok(generics::ContentItem::Counter(name, style))
                         }),
+                        #[cfg(any(feature = "gecko", feature = "servo-layout-2013"))]
                         "counters" => input.parse_nested_block(|input| {
                             let location = input.current_source_location();
                             let name = CustomIdent::from_ident(location, input.expect_ident()?, &[])?;
@@ -176,6 +177,7 @@ impl Parse for Content {
                     }?;
                     content.push(result);
                 },
+                #[cfg(any(feature = "gecko", feature = "servo-layout-2013"))]
                 Ok(&Token::Ident(ref ident)) => {
                     content.push(match_ignore_ascii_case! { &ident,
                         "open-quote" => generics::ContentItem::OpenQuote,
@@ -190,7 +192,7 @@ impl Parse for Content {
                         _ =>{
                             let ident = ident.clone();
                             return Err(input.new_custom_error(
-                                SelectorParseErrorKind::UnexpectedIdent(ident)
+                                StyleParseErrorKind::UnexpectedIdent(ident)
                             ));
                         }
                     });
